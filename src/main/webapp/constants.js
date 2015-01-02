14e25f14c29ee3a1fd2f0084a0851481114d13a1
aalcocer <ariel.alcocer@gmail.com>
2015-01-02 10:45:17 -0300

    Deshabilitando HTTPS.

--- src/main/webapp/constants.js
+++ src/main/webapp/constants.js
@@ -3,8 +3,8 @@
 /* Constants */
 
 var URL = {};
-URL.host = "https://172.31.80.5";
-URL.port = "7036";
+URL.host = "http://172.31.80.5";
+URL.port = "7026";
 
 luckynumbersApp.constant('USER_ROLES', {
 	'all': '*',
